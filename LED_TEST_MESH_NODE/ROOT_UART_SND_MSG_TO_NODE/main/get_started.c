8acbc459a64ab14d3c0bb44985ef49479239936f
myselfanand <121082777+myselfanand@users.noreply.github.com>
2022-12-21 10:31:36 +0530

    Add files via upload

---
+++ LED_TEST_MESH_NODE/ROOT_UART_SND_MSG_TO_NODE/main/get_started.c
@@ -0,0 +1,246 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "driver/uart.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
+#include "soc/uart_struct.h"
+#include "mdf_common.h"
+#include "mwifi.h"
+#include "driver/gpio.h"
+
+uint8_t* data1;
+uint8_t* data2;
+int len;
+uint8_t src_addr[MWIFI_ADDR_LEN] = {0x0};
+#define BUF_SIZE (1024)
+
+#define BLINK_GPIO  2 
+
+static const char *TAG = "get_started";
+
+///////////// GPIO BLINKING ////////////////
+// static void _blink() 
+//     {
+//      for (uint8_t i = 0; i < times; ++i)
+//      {
+//       gpio_set_level(BLINK_GPIO, 1);
+//       vTaskDelay(100 / portTICK_PERIOD_MS);
+//       gpio_set_level(BLINK_GPIO, 0);
+//       vTaskDelay(100 / portTICK_PERIOD_MS);
+//      }
+//    }
+
+//an example of echo test with hardware flow control on UART1
+static void echo_task()
+{
+    int z;
+    const int uart_num0 = UART_NUM_0;
+    uart_config_t uart_config0 = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,    //UART_HW_FLOWCTRL_CTS_RTS,
+        .rx_flow_ctrl_thresh = 122,
+    };
+          
+    //Configure UART0 parameters
+    uart_param_config(uart_num0, &uart_config0);
+
+    uart_set_pin(uart_num0, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    //Install UART driver (we don't need an event queue here)
+    //In this example we don't even use a buffer for sending data.
+    uart_driver_install(uart_num0, BUF_SIZE * 2, 0, 0, NULL, 0);
+
+    data1 = (uint8_t*) malloc(BUF_SIZE);
+    data2 = (uint8_t*) malloc(BUF_SIZE);
+
+    mdf_err_t ret                    = MDF_OK;
+    char *data                       = MDF_MALLOC(MWIFI_PAYLOAD_LEN);
+    size_t size                      = MWIFI_PAYLOAD_LEN;
+    
+    mwifi_data_type_t data_type      = {0};
+
+    while(1) 
+    {
+        //Read data from UART
+        len = uart_read_bytes(uart_num0, data1, BUF_SIZE, 20 / portTICK_RATE_MS);
+        if(len > 0)
+        {
+            //Write data back to UART
+            for(z = 0; z < len ; z++)
+            {
+                data2[z] = data1[z];
+            }
+            data2[len] = '\0';
+            uart_write_bytes(uart_num0, (const char*) data2, len);
+
+         //send msg to node device //
+         size = sprintf(data, "%s", (const char*)data2);
+         printf("Test: %s \n", data);
+
+         ret = mwifi_root_write(src_addr, 1, &data_type, data, size, true);
+         MDF_ERROR_CONTINUE(ret != MDF_OK, "mwifi_root_recv, ret: %x", ret);
+        }     
+    }
+}
+
+// static const char *TAG = "get_started";
+
+static void root_task(void *arg)
+{
+    mdf_err_t ret                    = MDF_OK;
+    char *data                       = MDF_MALLOC(MWIFI_PAYLOAD_LEN);
+    size_t size                      = MWIFI_PAYLOAD_LEN;
+    // uint8_t src_addr[MWIFI_ADDR_LEN] = {0x0};
+    mwifi_data_type_t data_type      = {0};
+
+    MDF_LOGI("Root is running");
+
+
+    for (int i = 0;; ++i) {
+        if (!mwifi_is_started()) {
+            vTaskDelay(500 / portTICK_RATE_MS);
+            continue;
+        }
+        
+        size = MWIFI_PAYLOAD_LEN;
+        memset(data, 0, MWIFI_PAYLOAD_LEN);
+        ret = mwifi_root_read(src_addr, &data_type, data, &size, portMAX_DELAY);
+        MDF_ERROR_CONTINUE(ret != MDF_OK, "<%s> mwifi_root_read", mdf_err_to_name(ret));
+
+        // print root receive mac addr & data(msg) to node device //
+        MDF_LOGI("Root receive, addr: " MACSTR ", size: %d, data: %s", MAC2STR(src_addr), size, data);
+        
+        // send msg to node device //
+        // size = sprintf(data, "%s", (const char*)data2);
+        // printf("Test: %s \n", data);
+        // ret = mwifi_root_write(src_addr, 1, &data_type, data, size, true);
+        // MDF_ERROR_CONTINUE(ret != MDF_OK, "mwifi_root_recv, ret: %x", ret);
+
+        // print data of root send mac addr & data(msg) to node device //
+        // MDF_LOGI("Root send, addr: " MACSTR ", size: %d, data: %s", MAC2STR(src_addr), size, data);  
+       
+    }
+
+    MDF_LOGW("Root is exit");
+
+    MDF_FREE(data);
+    vTaskDelete(NULL);
+}
+
+static void print_system_info_timercb(void *timer)
+{
+    uint8_t primary                 = 0;
+    wifi_second_chan_t second       = 0;
+    mesh_addr_t parent_bssid        = {0};
+    uint8_t sta_mac[MWIFI_ADDR_LEN] = {0};
+    wifi_sta_list_t wifi_sta_list   = {0x0};
+
+    esp_wifi_get_mac(ESP_IF_WIFI_STA, sta_mac);
+    esp_wifi_ap_get_sta_list(&wifi_sta_list);
+    esp_wifi_get_channel(&primary, &second);
+    esp_mesh_get_parent_bssid(&parent_bssid);
+
+    MDF_LOGI("System information, channel: %d, layer: %d, self mac: " MACSTR ", parent bssid: " MACSTR
+             ", parent rssi: %d, node num: %d, free heap: %u", primary,
+             esp_mesh_get_layer(), MAC2STR(sta_mac), MAC2STR(parent_bssid.addr),
+             mwifi_get_parent_rssi(), esp_mesh_get_total_node_num(), esp_get_free_heap_size());
+
+    for (int i = 0; i < wifi_sta_list.num; i++) {
+        MDF_LOGI("Child mac: " MACSTR, MAC2STR(wifi_sta_list.sta[i].mac));
+    }
+}
+
+static mdf_err_t wifi_init()
+{
+    mdf_err_t ret          = nvs_flash_init();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        MDF_ERROR_ASSERT(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    MDF_ERROR_ASSERT(ret);
+
+    MDF_ERROR_ASSERT(esp_netif_init());
+    MDF_ERROR_ASSERT(esp_event_loop_create_default());
+    MDF_ERROR_ASSERT(esp_wifi_init(&cfg));
+    MDF_ERROR_ASSERT(esp_wifi_set_storage(WIFI_STORAGE_FLASH));
+    MDF_ERROR_ASSERT(esp_wifi_set_mode(WIFI_MODE_STA));
+    MDF_ERROR_ASSERT(esp_wifi_set_ps(WIFI_PS_NONE));
+    MDF_ERROR_ASSERT(esp_mesh_set_6m_rate(false));
+    MDF_ERROR_ASSERT(esp_wifi_start());
+
+    return MDF_OK;
+}
+
+static mdf_err_t event_loop_cb(mdf_event_loop_t event, void *ctx)
+{
+    MDF_LOGI("event_loop_cb, event: %d", event);
+
+    switch (event) {
+        case MDF_EVENT_MWIFI_STARTED:
+            MDF_LOGI("MESH is started");
+            break;
+
+        case MDF_EVENT_MWIFI_PARENT_CONNECTED:
+            MDF_LOGI("Parent is connected on station interface");
+            break;
+
+        case MDF_EVENT_MWIFI_PARENT_DISCONNECTED:
+            MDF_LOGI("Parent is disconnected on station interface");
+            break;
+
+        default:
+            break;
+    }
+
+    return MDF_OK;
+}
+
+void app_main()
+{
+
+    gpio_pad_select_gpio(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    mwifi_init_config_t cfg = MWIFI_INIT_CONFIG_DEFAULT();
+    mwifi_config_t config   = {
+        .channel   = CONFIG_MESH_CHANNEL,
+        .mesh_id   = CONFIG_MESH_ID,
+        .mesh_type = CONFIG_DEVICE_TYPE,
+    };
+
+    /**
+     * @brief Set the log level for serial port printing.
+     */
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set(TAG, ESP_LOG_DEBUG);
+
+    /**
+     * @brief Initialize wifi mesh.
+     */
+    MDF_ERROR_ASSERT(mdf_event_loop_init(event_loop_cb));
+    MDF_ERROR_ASSERT(wifi_init());    
+    MDF_ERROR_ASSERT(mwifi_init(&cfg));
+    MDF_ERROR_ASSERT(mwifi_set_config(&config));
+    MDF_ERROR_ASSERT(mwifi_start());
+
+
+    xTaskCreate(root_task, "root_task", 4 * 1024,
+                    NULL, CONFIG_MDF_TASK_DEFAULT_PRIOTY, NULL);
+
+    TimerHandle_t timer = xTimerCreate("print_system_info", 10000 / portTICK_RATE_MS,
+                                       true, NULL, print_system_info_timercb);
+    xTimerStart(timer, 0);
+
+    //A uart read/write example without event queue;
+    xTaskCreate(echo_task, "uart_echo_task", 1024*2, NULL, 10, NULL);
+}
